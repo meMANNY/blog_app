6773c852b6aac38deab51d51d7b828e97c0cdd31
Aman Tripathi <amanttripathi02@gmail.com>
2024-04-18 22:11:32 +0530

    "created a form data"

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const express = require('express');
 
+const userRoute = require('./routes/user');
+
+
+
+
 const app = express();
 PORT  = 3000;
 
@@ -10,5 +15,10 @@ app.set('views', path.resolve('./views'));
 app.get('/', (req, res) => {
     res.render("home");
 })
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use('/user', userRoute);
 
 app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
