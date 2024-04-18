999a43b41f41a9b93bbb396bcd011b03fd17ab98
Aman Tripathi <amanttripathi02@gmail.com>
2024-04-18 18:08:47 +0530

    "made the first setup of the project"

---
+++ index.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+PORT  = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
