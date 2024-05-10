65d4f594ab3b5e23f4f3483fd23cfd2dd3742eac
Aman Tripathi <amanttripathi02@gmail.com>
2024-05-10 15:30:20 +0530

    "implemented multer for data storage"

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,37 @@
 const {Router} = require('express');
 
 const router = Router();
+const multer = require("multer");
+const path = require("path");
+
+const Blog = require('../models/blog');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.resolve(`./public/uploads/`))
+    },
+    filename: function(req,file,cb){
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random()*1E9)
+        cb(null,file.fieldname+'-'+ uniqueSuffix)
+    }
+})
+
+const upload = multer({storage: storage});
 
 router.get("/add-new", (req, res) => {
     return res.render("addBlog", {
         user: req.user,
     });
 });
+
+router.post("/", upload.single("coverImage"), async (req,res)=>{
+    const {title,body} = req.body;
+    const blog = await Blog.create({
+        body,
+        title,
+        createdBy: req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`,
+    })
+    return res.redirect(`/blog/${blog._id}`);
+})
 
 module.exports = router;
